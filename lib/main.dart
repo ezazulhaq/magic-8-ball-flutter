ef28b1a32933b3588d901247ee58cbad8e899b58
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-05-19 14:57:28 +0530

    Boss Level Challenge - Magic 8 Ball flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Magic8Ball(),
+      ),
+    );
+  }
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int clickImageBall = 1;
+
+  void getMagicBalls() {
+    setState(() {
+      clickImageBall = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                getMagicBalls();
+              },
+              child: Image.asset("images/ball$clickImageBall.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
